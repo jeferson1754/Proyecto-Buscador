60751c619721e0cdf160d6e1f8c08cb991fe373e
=Jeferson Vargas <=jeferson.vargas@inacapmail.cl>
2024-02-24 11:00:27 -0300

    Se optimizo y mejoro las consultas sql

--- Autor.php
+++ Autor.php
@@ -6,7 +6,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/autores.css?v=<?php echo time(); ?>">
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Buscador de Canciones</title>
 </head>
@@ -16,35 +15,69 @@
         <input type="search" name="key" placeholder="Buscar...">
         <button type="submit"><i class="fas fa-search"></i></button>
     </form>
-
-
     <div class="deudores">
         <?php
         require '../bd.php';
-
         $limit = 5;
+        $query = isset($_GET['key']) ? $_GET['key'] : '';
 
-        if (isset($_GET['key'])) {
-            // Recuperar el término de búsqueda
-            $query = $_GET['key'];
-        } else {
-            $query = "";
+        function displayTableop($sql, $limit, $conexion)
+        {
+            $result = mysqli_query($conexion, $sql);
+            if ($result && mysqli_num_rows($result) > 0) {
+                while ($mostrar = mysqli_fetch_array($result)) {
+                    $id_Registros = $mostrar['ID'];
+        ?>
+                    <tr>
+                        <td><?php echo $mostrar['Nombre'] ?></td>
+                        <td>OP <?php echo $mostrar['Opening'] ?></td>
+                        <td><?php echo $mostrar['Cancion'] ?></td>
+                        <td><?php echo $mostrar['Autor'] ?></td>
+                        <td>
+                            <div class="container" style="width: 100%; height: 100px;">
+                                <iframe src="/Anime/OP/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+                            </div>
+                        </td>
+                    </tr>
+                <?php
+                }
+            } else {
+                echo "<tr><td colspan='5' style='text-align:center'>No se encontraron resultados en la base de datos.</td></tr>";
+            }
         }
 
+        function displayTabled($sql, $limit, $conexion)
+        {
+            $result = mysqli_query($conexion, $sql);
+            if ($result && mysqli_num_rows($result) > 0) {
+                while ($mostrar = mysqli_fetch_array($result)) {
+                    $id_Registros = $mostrar['ID'];
+                ?>
+                    <tr>
+                        <td><?php echo $mostrar['Nombre'] ?></td>
+                        <td>ED <?php echo $mostrar['Ending'] ?></td>
+                        <td><?php echo $mostrar['Cancion'] ?></td>
+                        <td><?php echo $mostrar['Autor'] ?></td>
+                        <td>
+                            <div class="container" style="width: 100%; height: 100px;">
+                                <iframe src="/Anime/ED/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" style="width: 100%; height: 100%;"></iframe>
+                            </div>
+                        </td>
+                    </tr>
+        <?php
+                }
+            } else {
+                echo "<tr><td colspan='5' style='text-align:center'>No se encontraron resultados en la base de datos.</td></tr>";
+            }
+        }
 
         ?>
         <div class="menu-container">
             <div class="menu-options">
-                <div class="menu-option" onclick="showOption(1)">
-                    Autores
-                    <div class="menu-line"></div>
+                <div class="menu-option" onclick="showOption(1)">Autores<div class="menu-line"></div>
                 </div>
-                <div class="menu-option" onclick="showOption(2)">
-                    Animes
-                </div>
-                <div class="menu-option" onclick="showOption(3)">
-                    Canciones
-                </div>
+                <div class="menu-option" onclick="showOption(2)">Animes</div>
+                <div class="menu-option" onclick="showOption(3)">Canciones</div>
             </div>
         </div>
         <div class="info-container active" id="info-option-1">
@@ -52,7 +85,6 @@
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Opening</th>
                         <th>Cancion</th>
@@ -60,31 +92,8 @@
                     </tr>
                 </thead>
                 <?php
-
-                $sql = "SELECT * FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE autor.Autor LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>OP <?php echo $mostrar['Opening'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/OP/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT op.*, autor.Autor FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE autor.Autor LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
+                displayTableop($sql, $limit, $conexion);
                 ?>
             </table>
             <br>
@@ -92,7 +101,6 @@
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Ending</th>
                         <th>Cancion</th>
@@ -100,31 +108,8 @@
                     </tr>
                 </thead>
                 <?php
-
-                $sql = "SELECT * FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE autor.Autor LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>ED <?php echo $mostrar['Ending'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/ED/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT ed.*, autor.Autor FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE autor.Autor LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
+                displayTabled($sql, $limit, $conexion);
                 ?>
             </table>
         </div>
@@ -133,7 +118,6 @@
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Opening</th>
                         <th>Cancion</th>
@@ -141,39 +125,15 @@
                     </tr>
                 </thead>
                 <?php
-
-                $sql = "SELECT * FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE op.Nombre LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>OP <?php echo $mostrar['Opening'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/OP/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT op.*, autor.Autor FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE op.Nombre LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
+                displayTableop($sql, $limit, $conexion);
                 ?>
             </table>
-            </br>
+            <br>
             <h2>Endings</h2>
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Ending</th>
                         <th>Cancion</th>
@@ -181,31 +141,8 @@
                     </tr>
                 </thead>
                 <?php
-
-                $sql = "SELECT * FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE ed.Nombre LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>ED <?php echo $mostrar['Ending'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/ED/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT ed.*, autor.Autor FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE ed.Nombre LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
+                displayTabled($sql, $limit, $conexion);
                 ?>
             </table>
         </div>
@@ -214,7 +151,6 @@
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Opening</th>
                         <th>Cancion</th>
@@ -222,32 +158,8 @@
                     </tr>
                 </thead>
                 <?php
-
-
-                $sql = "SELECT * FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE op.Cancion LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>OP <?php echo $mostrar['Opening'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/OP/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT op.*, autor.Autor FROM `op` JOIN autor ON op.ID_Autor=autor.ID WHERE op.Cancion LIKE '%$query%' ORDER BY `op`.`ID` DESC limit $limit";
+                displayTableop($sql, $limit, $conexion);
                 ?>
             </table>
             <br>
@@ -255,7 +167,6 @@
             <table id="example" style="width:100%">
                 <thead>
                     <tr>
-
                         <th>Nombre</th>
                         <th>Ending</th>
                         <th>Cancion</th>
@@ -263,30 +174,8 @@
                     </tr>
                 </thead>
                 <?php
-                $sql = "SELECT * FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE ed.Cancion LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
-                //echo $sql;
-
-                $result = mysqli_query($conexion, $sql);
-
-
-                while ($mostrar = mysqli_fetch_array($result)) {
-                    $id_Registros = $mostrar['ID'];
-                ?>
-
-
-                    <tr>
-                        <td><?php echo $mostrar['Nombre'] ?></td>
-                        <td>ED <?php echo $mostrar['Ending'] ?></td>
-                        <td><?php echo $mostrar['Cancion'] ?></a></td>
-                        <td><?php echo $mostrar['Autor'] ?></td>
-                        <td>
-                            <div class="container" style="width: 100%; height: 100px;">
-                                <iframe src="/Anime/ED/ejemplo.php?id=<?php echo $id_Registros; ?>" frameborder="0" ñ style="width: 100%; height: 100%;"></iframe>
-                            </div>
-                    </tr>
-                    <!--Ventana Modal para Actualizar--->
-                <?php
-                }
+                $sql = "SELECT ed.*, autor.Autor FROM `ed` JOIN autor ON ed.ID_Autor=autor.ID WHERE ed.Cancion LIKE '%$query%' ORDER BY `ed`.`ID` DESC limit $limit";
+                displayTabled($sql, $limit, $conexion);
                 ?>
             </table>
         </div>
@@ -296,15 +185,12 @@
 
         function showOption(option) {
             if (option !== selectedOption) {
-                // Mover la línea debajo de la opción seleccionada
                 const menuLine = document.querySelector('.menu-line');
                 menuLine.style.transform = `translateX(${(option - 1) * 128}%)`;
 
-                // Ocultar la información de la opción anterior
                 const prevInfoContainer = document.querySelector(`#info-option-${selectedOption}`);
                 prevInfoContainer.classList.remove('active');
 
-                // Mostrar la información de la opción seleccionada
                 const currentInfoContainer = document.querySelector(`#info-option-${option}`);
                 currentInfoContainer.classList.add('active');
 
@@ -312,7 +198,6 @@
             }
         }
 
-        // Al cargar la página, mostrar la primera opción por defecto
         showOption(selectedOption);
     </script>
 </body>
